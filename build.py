438aafd569dd8fadcf372dd2cdebdeef05203121
Thomas Hope <contact@thomashope.co.uk>
2025-06-05 13:53:07 +0200

    WIP generating archive page

--- build.py
+++ build.py
@@ -8,10 +8,11 @@ import re
 
 src_dir = 'src'
 dest_dir = 'public'
-header = open(os.path.join(src_dir, 'res', 'header.template')).read()
-footer = open(os.path.join(src_dir, 'res', 'footer.template')).read()
+page_template = open(os.path.join(src_dir, 'res', 'template.html')).read()
 
 md = markdown.Markdown(extensions=['fenced_code', 'meta', 'tables', 'footnotes'])
+
+pages = []
 
 def print_warning(str):
 	print('\033[93m' + 'WARNING: ' + str + '\033[0m')
@@ -58,6 +59,9 @@ def build_markdown_files():
 				text = text.replace(match, get_macro_expansion(macro))
 			html = md.convert(text)
 
+		if 'published' not in md.Meta or md.Meta['published'][0].lower() != 'true':
+			continue
+
 		title = 'Thomas Hope'
 		if 'title' in md.Meta:
 			title = md.Meta['title'][0]
@@ -74,6 +78,10 @@ def build_markdown_files():
 		if len(description) > max_description_length:
 			print_warning('Description should be kept under ' + str(max_description_length) + ' characters! Length is ' + str(len(description)))
 
+		date = 'YYYY-MM-DD'
+		if 'date' in md.Meta:
+			date = md.Meta['date'][0]
+
 		site_path = os.path.splitext(path[len(src_dir)+1:])[0] + '.html'
 		destination = os.path.join(dest_dir, site_path)
 		date_edited = run_cmd(['git','log','-1','--pretty="%aD"', path]).strip(' \n"')
@@ -82,24 +90,52 @@ def build_markdown_files():
 
 		url = 'https://thomashope.xyz/' + site_path
 
+		pages.append([date, site_path, title])		
+
 		with open(destination, 'w') as file:
-			file.write(header
+			file.write(page_template
 				.replace('$$TITLE$$', title)
 				.replace('$$DESCRIPTION$$', description)
 				.replace('$$URL$$', url)
-				.replace('$$IMAGE$$', image_path))
-			file.write(html)
-			file.write(footer
+				.replace('$$IMAGE$$', image_path)
+				.replace('$$CONTENT$$', html)
 				.replace('$$DATE_EDITED$$', date_edited)
 				.replace('$$GIT_HISTORY_LINK$$', git_history_link))
 
 		md.reset()
+
+def build_archive():
+	# Sort pages by date descending (assuming ISO format: YYYY-MM-DD)
+	sorted_pages = sorted(pages, key=lambda p: p[0], reverse=True)
+
+	items = []
+	for date, site_path, title in sorted_pages:
+		item_html = f'<li><time>{date}</time><a href="../{site_path}">{title}</a></li>'
+		items.append(item_html)
+
+	content = '<ul style="list-style: none; padding: 0;">\n' + '\n'.join(items) + '\n</ul>'
+
+	archive_html = page_template \
+		.replace('$$TITLE$$', 'Thomas Hope') \
+		.replace('$$DESCRIPTION$$', 'Homepage') \
+		.replace('$$URL$$', 'https://thomashope.xyz/') \
+		.replace('$$IMAGE$$', '/res/diving.jpg') \
+		.replace('$$CONTENT$$', content) \
+		.replace('$$DATE_EDITED$$', 'TODO: insert date edited') \
+		.replace('$$GIT_HISTORY_LINK$$', 'TODO: insert link to file in git')
+
+	destination = os.path.join(dest_dir, 'archive', 'index.html')
+	create_dirs(os.path.split(destination)[0])
+
+	with open(destination, 'w') as file:
+		file.write(archive_html)
 
 def main():
 	print('Starting build...')
 	create_dirs(dest_dir)
 	mirror_files_with_extensions(['html', 'css', 'jpg', 'jpeg', 'png', 'webm', 'mp4', 'ico', 'svg', 'webmanifest'])
 	build_markdown_files()
+	build_archive()
 	print('Done!')
 
 if __name__ == '__main__':
